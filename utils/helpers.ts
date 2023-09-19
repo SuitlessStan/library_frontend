9252554707ca1cf27ffdcc062f5ce30229001049
Issam Olwan <lands.of.48@gmail.com>
2023-09-19 21:54:20 +0300

    edit helper methods

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,7 +1,9 @@
+import { Book } from "./types"
+
 export const scrollToTop = () => {
   window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
 }
 
-export function getUniqueListBy<T>(arr: T[], key: string): T[] {
-  return [...new Map(arr.map((item) => [item[key], item])).values()]
+export function getUniqueListBy<T>(arr: Book[], key: string): Book[] {
+  return [...new Map(arr.map((item: any) => [item[key], item])).values()]
 }
